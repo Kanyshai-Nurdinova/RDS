d4d80675aa24b023cc2da74c35567091aa258e11
Kanyshai-Nurdinova <79550683+Kanyshai-Nurdinova@users.noreply.github.com>
2025-02-12 20:03:51 -0500

    Added variables

---
+++ RDS/main.tf
@@ -0,0 +1,48 @@
+resource "aws_rds_cluster" "aurora_cluster" {
+  cluster_identifier      = var.identifier
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  database_name          = var.database_name
+  master_username        = var.master_username
+  master_password        = random_password.password.result
+  skip_final_snapshot    = true
+  backup_retention_period = 7
+}
+
+# Writer instance
+resource "aws_rds_cluster_instance" "writer" {
+  identifier         = var.writer_identifier
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = var.writer_instance_class
+  engine            = aws_rds_cluster.aurora_cluster.engine
+  engine_version    = aws_rds_cluster.aurora_cluster.engine_version
+  publicly_accessible = true
+}
+
+# Reader instances
+resource "aws_rds_cluster_instance" "reader1" {
+  identifier         = var.reader1_identifier
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = var.reader1_instance_class
+  engine            = aws_rds_cluster.aurora_cluster.engine
+  engine_version    = aws_rds_cluster.aurora_cluster.engine_version
+  publicly_accessible = true
+}
+
+resource "aws_rds_cluster_instance" "reader2" {
+  identifier         = var.reader2_identifier
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = var.reader2_instance_class
+  engine            = aws_rds_cluster.aurora_cluster.engine
+  engine_version    = aws_rds_cluster.aurora_cluster.engine_version
+  publicly_accessible = true
+}
+
+resource "aws_rds_cluster_instance" "reader3" {
+  identifier         = var.reader3_identifier
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = var.reader3_instance_class
+  engine            = aws_rds_cluster.aurora_cluster.engine
+  engine_version    = aws_rds_cluster.aurora_cluster.engine_version
+  publicly_accessible = true
+}
---
+++ RDS/variable.tf
@@ -0,0 +1,52 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "identifier"{
+
+}
+variable "engine"{
+
+}
+variable "engine_version"{
+
+}
+variable "database_name"{
+
+}
+variable "master_username"{
+
+}
+
+variable "writer_identifier"{
+
+}
+
+variable "writer_instance_class" {
+
+}
+variable "reader1_identifier"{
+
+}
+variable "reader1_instance_class"{
+
+}
+
+variable "reader2_identifier"{
+
+}
+variable "reader2_instance_class"{
+  
+}
+
+variable "reader3_identifier"{
+
+}
+variable "reader3_instance_class"{
+  
+}
+
+variable "writer_name"{}
+variable "reader1_name"{}
+variable "reader2_name"{}
+variable "reader3_name"{}
