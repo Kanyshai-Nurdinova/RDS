57b9f4b72d371b54a971310a71a250a8c19fc690
Kanyshai-Nurdinova <79550683+Kanyshai-Nurdinova@users.noreply.github.com>
2025-02-13 11:43:18 -0500

    added default value

--- RDS/variable.tf
+++ RDS/variable.tf
@@ -3,50 +3,70 @@ variable "region" {
 }
 
 variable "identifier"{
+  default = "static-member-1"
 
 }
 variable "engine"{
+  default = "aurora_db"
 
 }
 variable "engine_version"{
+  default = "14.5"
 
 }
 variable "database_name"{
+  default = "db"
 
 }
 variable "master_username"{
+  default = "master"
 
 }
 
 variable "writer_identifier"{
+  default = "writer-1"
 
 }
 
 variable "writer_instance_class" {
+  default = "db.r5.2xlarge"
 
 }
 variable "reader1_identifier"{
+  default = "reader1"
 
 }
 variable "reader1_instance_class"{
+  default = "db.r5.2xlarge"
 
 }
 
 variable "reader2_identifier"{
-
+   default = "reader1"
 }
 variable "reader2_instance_class"{
+  default = "db.r5.2xlarge"
   
 }
 
 variable "reader3_identifier"{
+   default = "reader1"
 
 }
 variable "reader3_instance_class"{
+  default = "db.r5.2xlarge"
   
 }
 
-variable "writer_name"{}
-variable "reader1_name"{}
-variable "reader2_name"{}
-variable "reader3_name"{}
+variable "writer_name"{
+   default = "writer"
+}
+variable "reader1_name"{
+   default = "reader1"
+}
+variable "reader2_name"{
+   default = "reader2"
+}
+variable "reader3_name"{
+   default = "reader3"
+}
